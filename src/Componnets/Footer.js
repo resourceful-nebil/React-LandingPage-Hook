199cffee4bf8aae3af928fd656ea69e7f904e43f
Nebil <resourceful.nebil@gmail.com>
2023-04-23 16:07:24 +0300

    first commit

---
+++ src/Componnets/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../App.css';
+
+function Footer() {
+  return (
+    <div className='footer'>
+      <p>Shopping Site 2023 &copy;</p>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/Componnets/Navabar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../App.css';
+
+function Navabar() {
+  return (
+    <div className='navbar'>
+      <h1>Shopping Site</h1>
+      
+    </div>
+  )
+}
+
+export default Navabar
